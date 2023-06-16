405973d9dc87bf3fadac7e1b048c8d94172ca756
RicardoPLins <79940823+RicardoPLins@users.noreply.github.com>
2023-06-15 23:07:46 -0300

    Add files via upload

---
+++ src/app/salao/listagem-salao/listagem-salao.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Salao} from '../../shared/modelo/salao';
+import { SalaoService } from 'src/app/shared/services/salao.service';
+import { SALOES } from 'src/app/shared/modelo/SALOES';
+import {map, Observable} from "rxjs";
+import {SalaoFirestoreService} from "../../shared/services/salao-firestore.service";
+
+@Component({
+  selector: 'app-listagem-salao',
+  templateUrl: './listagem-salao.component.html',
+  styleUrls: ['./listagem-salao.component.css']
+})
+export class ListagemSalaoComponent {
+
+  saloes: Observable<Salao[]>;
+  quantidadeDeSalao: Observable<number>;
+
+
+  constructor(private salaoService: SalaoFirestoreService) {
+    this.saloes = salaoService.listar();
+    this.quantidadeDeSalao = this.saloes.pipe(map(saloes => saloes.length));
+
+  }
+
+  excluir(salaoARemover: Salao): void{
+    if (salaoARemover.id){
+      this.salaoService.apagar(salaoARemover.id).subscribe(
+        salaoRemovido => {}
+      );
+    }
+  }
+
+}
